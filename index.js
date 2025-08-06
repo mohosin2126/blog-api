8f53931758f8f3bd3d22baec1cf53523b3fb0feb
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2025-08-06 15:02:30 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+const PORT = process.env.PORT || 5000;
 const blogRoutes = require('./routes/index');
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/api', blogRoutes);
 
-const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Blog API is running successfully!');
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
